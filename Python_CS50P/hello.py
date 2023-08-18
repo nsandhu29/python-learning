5e1282c1e9a2db09e6d918ec47b69c314834ee99
Navjot Singh <navjotsingh@Navjots-MacBook-Pro.local>
2023-08-18 11:52:31 -0500

    First lecture of CS50P

---
+++ Python_CS50P/hello.py
@@ -0,0 +1,11 @@
+# Ask user for their name
+name = input("What's your name? ").strip().title()
+
+# Remove whitespace from str and capitalize
+#name = name.strip().title()
+
+# Split user's name into first name and last name
+first, last = name.split(' ')
+
+# Say hello to user
+print(f'hello, {first}')
